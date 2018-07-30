4a651f6257e5bf8f0fb9c21213c188e27bea168a
Kevin Cauto <kevinmcauto@gmail.com>
2018-07-30 18:41:51 -0400

    Fixed ID digital edition footer.

--- client/src/templates/id_digital.js
+++ client/src/templates/id_digital.js
@@ -288,10 +288,7 @@ export function end(fields) {
               <a href="${link}" style="text-decoration:none; color:#9d0707; font-size:13px; line-height:21px;" target="_blank"><strong>EXPLORE</strong></a></div>
       </td>
   </tr>
-</tbody>
-</table>
-</td>
-</tr>
+
 <tr>
 <td bgcolor="#ffffff" colspan="6" height="5" style="font-family:Arial, sans-serif; font-size:11px; text-align:center; color:#666666; font-size:10px;" valign="middle">
 <div style="margin:2px 0 12px 0; clear:both;">
@@ -311,7 +308,7 @@ Connect with <em>Inside Dentistry</em></div>
 
 <tr>
 <td align="center" style="font-family:Arial, sans-serif; font-size:11px; text-align:center; color:#666666; font-size:10px;" valign="middle">
-        <br>
+      
 You received this e-mail because you are a customer of <a href="https://www.aegisdentalnetwork.com" target="_blank" style="color:#666 !important; text-decoration:none;">AEGIS Dental Network</a> | <em>Inside Dentistry</em>.<br />To ensure delivery, please add <a href="mailto:reply-289795@news.aegispublications.com" style="color:#666 !important; text-decoration:none;" target="_blank">reply-289795@news.aegispublications.com</a> to your address book.<br />
 %%PLUGIN_UNSUBSCRIBE: 1654490-UNSUBSCRIBE%% <br />
        <em>Inside Dentistry</em>, an AEGIS Publication | 104 Pheasant Run, Suite 105 | Newtown, PA 18940 | <a href="https://www.aegisdentalnetwork.com/privacy-policy" target="_blank" style="color:#666; text-decoration:none;">Privacy Policy</a><br><br></td>
